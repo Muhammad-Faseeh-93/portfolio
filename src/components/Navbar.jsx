5cd24e9f8a644f04709ffae8edfdf790f69f631a
Muhammad Faseeh <faseehsheikh93@gmail.com>
2023-11-03 12:30:37 +0500

    message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-scroll";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
+  
 
   const links = [
     {
@@ -33,6 +34,7 @@ const Navbar = () => {
       <div>
         <h1 className="text-4xl font-signature ml-2 font-5xl ">Faseeh</h1>
       </div>
+      
       <ul className="hidden md:flex">
         {links.map(({ id, link }) => (
           <li
@@ -45,19 +47,24 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+      
       <div
         onClick={() => setNav(!nav)}
         className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden "
       >
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
+      
+      
 
       {nav && (
+        
         <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-[#0a192f] ">
+          
           {links.map(({ id, link }) => (
             <li
               key={id}
-              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+              className="px-4 cursor-pointer capitalize py-6  text-4xl "
             >
               <Link
                 onClick={() => setNav(!nav)}
@@ -71,6 +78,7 @@ const Navbar = () => {
           ))}
         </ul>
       )}
+      
     </div>
   );
 };
